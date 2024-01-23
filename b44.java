23abd2d8aa76e3de98cec856159ffad812b4558e
Windsoor201 <155503450+Windsoor201@users.noreply.github.com>
2024-01-24 06:27:59 +1300

    Update b44.java

--- b44.java
+++ b44.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Main {
   public static void main(String[] args) {
     ArrayList<String> cars = new ArrayList<String>();
-    cars.add("Vlv");
+    cars.add("Vv");
     cars.add("N");
     cars.add("Fod");
     cars.add("Maza");
