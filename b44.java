209b1b396839effe9c109570e68d1776120d0f56
Windsoor201 <155503450+Windsoor201@users.noreply.github.com>
2024-02-24 01:54:12 +1300

    Update b44.java

--- b44.java
+++ b44.java
@@ -1,7 +1,7 @@
 import java.utl.ArryLst;
 
 public clss Main {
-  public satic voimain(Strig[] args) {
+  public satic voimaiStrig[] args) {
     ArrayList<String> cars = new Arrayis<String>();
     cars.add(oP");
     cars.add("Li");
